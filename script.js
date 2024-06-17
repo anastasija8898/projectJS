7c72945bd4a73329f18a2a56161e69c3eec42f49
Anastasiia <anastasija8898@gmail.com>
2024-06-17 11:20:10 +0200

    prompt method

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 "use strict";
 
-const category = "toys";
-console.log(`https://url.com/${category}`);
-("");
+// const category = "toys";
+// console.log(`https://url.com/${category}`);
+// ("");
+
+let numberOfFilms = prompt("How mane films have you watched?", 1);
+let personalMovieDB = {
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  private: false,
+};
+let movie = prompt("What was the latest movie?");
+let rating = prompt("Rating of this movie: ");
+personalMovieDB.movies[movie] = rating;
+
+console.log(numberOfFilms);
+console.log(personalMovieDB);
