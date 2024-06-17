02b8d7acd8128ba4187c74b7d715c519b737c600
Anastasiia <anastasija8898@gmail.com>
2024-06-17 11:59:14 +0200

    conditions

--- script.js
+++ script.js
@@ -4,17 +4,63 @@
 // console.log(`https://url.com/${category}`);
 // ("");
 
-let numberOfFilms = prompt("How mane films have you watched?", 1);
-let personalMovieDB = {
-  count: numberOfFilms,
-  movies: {},
-  actors: {},
-  genres: [],
-  private: false,
-};
-let movie = prompt("What was the latest movie?");
-let rating = prompt("Rating of this movie: ");
-personalMovieDB.movies[movie] = rating;
+//  <======== LESSON 18 ========>
+//
+// let numberOfFilms = +prompt("How mane films have you watched?", 1);
+// let personalMovieDB = {
+//   count: numberOfFilms,
+//   movies: {},
+//   actors: {},
+//   genres: [],
+//   private: false,
+// };
+// let movie = prompt("What was the latest movie?");
+// let rating = prompt("Rating of this movie: ");
+// personalMovieDB.movies[movie] = rating;
 
-console.log(numberOfFilms);
-console.log(personalMovieDB);
+// console.log(numberOfFilms);
+// console.log(personalMovieDB);
+
+//  <======== LESSON 20 ========>
+// console.log( NaN || 2 || undefined ); // 2
+
+// console.log( NaN && 2 && undefined ); // NaN
+
+// console.log( 1 && 2 && 3 ); // 3
+
+// console.log( !1 && 2 || !3 ); // false
+
+// console.log( 25 || null && !3 ); // 25
+
+// console.log( NaN || null || !3 || undefined || 5); // 5
+
+// console.log( NaN || null && !3 && undefined || 5); // 5
+
+// console.log( 5 === 5 && 3 > 1 || 5); // true
+
+// const hamburger = 3;
+// const fries = 3;
+// const cola = 0;
+// const nuggets = 2;
+
+// if (hamburger === 3 && cola || fries === 3 && nuggets) {
+//    console.log('Done!')
+// }
+
+// let hamburger;
+// const fries = NaN;
+// const cola = 0;
+// const nuggets = 2;
+
+// if (hamburger || cola || fries === 3 || nuggets) {
+//    console.log('Done!!!')
+// }
+
+// let hamburger;
+// const fries = NaN;
+// const cola = 0;
+// const nuggets = 2;
+
+// if (hamburger && cola || fries === 3 && nuggets) {
+//    console.log('Done!')
+// }
